6359596734d711f375f7eec1d1c9578f6c7241f2
Ma Chun Hung <hungchai08@yahoo.com.hk>
2015-10-17 23:35:22 +0800

    change get stock list url

--- app.js
+++ app.js
@@ -6,27 +6,56 @@ var parallel = require('co-parallel');
 var util = require('util');
 var buf = require("buffer");
 var request = require('request');
+var cheerio = require('cheerio');
+var ent = require('ent');
+var _ = require("underscore");
 
 global.mongoURI = global.config.mongoDbConn;
 
 //mongoose.connect(config[config.mongoDbConn[0]].URI);
 //sss
 //step 1
-var stockListURL = 'https://api.investtab.com/api/search?limit=5000&query=hk&chart_only=false&type=stock';
+//var stockListURL = 'https://api.investtab.com/api/search?limit=3000&query=hk&chart_only=false&type=stock';
+var stockListURL = "http://money18.on.cc/js/daily/stocklist/stockList_secCode.js"
 var stockMinutesQuoteURL = 'http://hkej.m-finance.com/charting/tomcat/mfchart?code=%s&period=1min&frame=72+HOUR';
 var stockHistDayQuoteURL = 'http://hkej.m-finance.com/charting/tomcat/mfchart';
 var stockTodayQuoteURL = 'http://hkej.m-finance.com/charting/tomcat/todaydata';
 var stockInfoURL = 'https://api.investtab.com/api/quote/%s/info';
 // registering remote methods
+//
+//function getStockList() {
+//    return function (callback) {
+//        request(stockListURL, function (error, response, body) {
+//            callback(error, JSON.parse(body));
+//        });
+//
+//    };
+//};
 
 function getStockList() {
     return function (callback) {
         request(stockListURL, function (error, response, body) {
-            callback(error, JSON.parse(body));
+            var $ = cheerio.load(body);
+            var stocks = [];
+            var M18={};
+            M18.list = {
+                add: function(symbol, chiName, engName) {
+                    var stock = {};
+                    stock.symbol = symbol+':HK';
+                    stock.sc = chiName;
+                    stock.en = engName;
+                    stocks.push(stock);
+                }
+            };
+            eval(ent.decode(body));
+            callback(error, stocks);
+
         });
 
     };
 };
+
+
 //e.g: stockSymbol = 00700.HK
 function getStockInfo(stockSymbol) {
     return function (callback) {
@@ -93,21 +122,37 @@ function saveStockDayHistQuoteMongo(stockDayQuoteList, db) {
         var lastupdate = new Date();
         var stockDayQuoteCollection = db.collection('stockDayQuote');
 
+        var checkComplete = _.after(data.length,
+            function() {
+                if (errmsg != null) {
+                    callback(new Error(errmsg), '');
+                } else {
+                    callback(null, 'finished saveStockDayHistQuoteMongo')
+                }
+            }
+        );
+        var errmsg;
         for (var i = 0, len = data.length; i < len; i++) {
 
             var stocksymbol = data[i].symbol;
             var stockDataset = data[i].dataset;
             console.log('symbol:' + data[i].symbol);
-            if (stockDataset != null) {
-                var bulk = stockDayQuote.initializeUnorderedBulkOp({
+            if (stockDataset != null && stockDataset.length > 0) {
+                var bulk = stockDayQuoteCollection.initializeUnorderedBulkOp({
                     useLegacyOps: true
                 });
                 for (var j = 0; j < stockDataset.length; j++) {
-                    var stockdaydata = stockDataset[j];
+
+                    var stockdaydata = {};
                     stockdaydata.symbol = stocksymbol;
+                    stockdaydata.date = new Date(stockDataset[j].Date);
+                    stockdaydata.high = stockDataset[j].High;
+                    stockdaydata.low = stockDataset[j].Low;
+                    stockdaydata.open = stockDataset[j].Open;
+                    stockdaydata.turnover = stockDataset[j].Turnover;
                     //batch.insert(data[i]);
                     bulk.find({
-                        $and: [{symbol: stocksymbol}, {Date: stockdaydata.Date}]
+                        $and: [{symbol: stocksymbol}, {date: stockdaydata.date}]
     
                     }).upsert().replaceOne(
                         stockdaydata
@@ -116,10 +161,17 @@ function saveStockDayHistQuoteMongo(stockDayQuoteList, db) {
                 }
                 bulk.execute(function (err, result) {
                     console.log(result.nInserted);
-                    callback(err, result);
+                    if (!err)
+                        errmsg += err.message;
+                    checkComplete;
                 });
+            }else
+            {
+                checkComplete;
             }
+
         }
+
     }
 }
 function saveStockListMongo(stocks, db) {
@@ -225,16 +277,16 @@ function saveStockInfoMongo(stockInfos, db) {
 MongoClient.connect(global.mongoURI, function (err, db) {
     co(function*() {
 
-
-        var stocks = (yield getStockList());
-        //var saveStocks = yield saveStockListMongo(stocks, db);
+        var stocks = yield getStockList();
+        //var stocks = (yield getStockList());
+        var saveStocks = yield saveStockListMongo(stocks, db);
         //stocks = stocks.slice(0,6);
-        //var getStockInfoMap = stocks.map(function (stock) {
-        //    return getStockInfo(stock.symbol);
-        //});
-        //var stockInfos = yield parallel(getStockInfoMap, 20);
-        //var saveStockInfos = yield saveStockInfoMongo(stockInfos, db)
-        ////
+        var getStockInfoMap = stocks.map(function (stock) {
+            return getStockInfo(stock.symbol);
+        });
+        var stockInfos = yield parallel(getStockInfoMap, 20);
+        var saveStockInfos = yield saveStockInfoMongo(stockInfos, db)
+        
 
         var getStockDayHistQuoteMap = stocks.map(function (stock) {
             return getstockHistDayQuoteList(stock.symbol)
@@ -242,7 +294,7 @@ MongoClient.connect(global.mongoURI, function (err, db) {
         })
         var stockDayHistQuote = yield parallel(getStockDayHistQuoteMap, 20);
         var saveStockDayQuotes = yield saveStockDayHistQuoteMongo(stockDayHistQuote, db);
-
+        //process.exit();
         //var res = yield parallel(p2, 4);
         //console.log(JSON.stringify(res))
 
