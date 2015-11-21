2a2799b3daab599fe60f4553538ee5e8adf518f4
Ma Chun Hung <hungchai08@yahoo.com.hk>
2015-11-21 14:13:11 +0800

    recontruct the program

--- app_new.js
+++ app_new.js
@@ -3,6 +3,7 @@ global.mongoose = require('mongoose');
 global.MongoClient = require('mongodb').MongoClient;
 global.co = require('co');
 global.parallel = require('co-parallel');
+global.thunkify = require('thunkify');
 global.util = require('util');
 global.request = require('request');
 global.ent = require('ent');
@@ -26,19 +27,32 @@ require('./Schema/stockDayQuoteSchema.js')();
 var argv1 = process.argv[2];
 
 co(function*() {
-
+    //step 1: load live stock list
     var stockSymbols = yield money18Api.getHKLiveStockList();
     yield nodeStockSpiderDAO.saveStockListMongo(global.mongoose, stockSymbols)
 
+    //step 2: load stock historical quotes
     if (argv1 == null) {
         var getStockDayHistQuoteMap = stockSymbols.map(function (stock) {
             return hkejApi.getstockHistDayQuoteList(stock.symbol)
-
         })
         var stockDayHistQuote = yield parallel(getStockDayHistQuoteMap, 20);
         var saveStockDayHistQuotes = yield nodeStockSpiderDAO.saveStockDayHistQuoteMongo(global.mongoose, stockDayHistQuote);
     }
+    //step 3: load stock today quotes
+    var getstockTodayQuoteListMap = stockSymbols.map(function (stock) {
+        return hkejApi.getstockTodayQuoteList(stock.symbol);
+    });
 
+    var stockTodayQuotes = yield parallel(getstockTodayQuoteListMap, 20);
+
+    var saveStockTodayQuoteMap = stockTodayQuotes.map(
+        function (stockTodayQuote) {
+            return nodeStockSpiderDAO.saveStockTodayQuote(mongoose, stockTodayQuote)
+        }
+    )
+
+    var saveStockTodayQuotes = yield parallel(saveStockTodayQuoteMap, 5);
 
 }).then
 (function (val) {
