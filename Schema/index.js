881f782c6e44a16c90a348d8ef8e36f44fb037bd
Tom ma <hungchai.nodejs@gmail.com>
2015-12-15 06:09:26 +0000

    minor

--- Schema/index.js
+++ Schema/index.js
@@ -2,6 +2,8 @@ var stockProfileSchema = require('./stockProfileSchema');
 var stockDayQuoteSchema = require('./stockDayQuoteSchema');
 var stockQuotesArray = require("./stockQuotesArray");
 
+
+
 module.exports.stockProfileSchema = stockProfileSchema();
 module.exports.stockDayQuoteSchema = stockDayQuoteSchema();
 module.exports.stockQuotesArray = stockQuotesArray();
